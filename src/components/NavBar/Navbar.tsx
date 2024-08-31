779c223f3f5d7c278e29d46244feb3659f355bc4
iara aquiere <iara-victoria@hotmail.com>
2024-08-31 17:36:11 -0300

    Navbar arreglos vicho

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -6,33 +6,27 @@ import "./Navbar.css";
 
 function Navbar() {
   const logueado = userStore(state => state.logueado)
-
   return (
-    
       <header>
         { logueado ? 
          <nav className="navbar navbar-expand-lg bg-body-tertiary">
          <div className="container">
          <img className="logo-w me-2" src={logo} alt="logo" />
-           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-             <span className="navbar-toggler-icon "></span>
+           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+             <span className="navbar-toggler-icon"></span>
            </button>
-           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-           <ul className="navbar-nav">
-                <li className="nav-item">
+           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+             <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-center">
+             <li className="nav-item">
                   <Link className="nav-link" to="/ordenes">Ordenes</Link>
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link" to="/stock">Stock</Link>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/logout"><FaUserCircle />Salir</Link>
-                </li> 
-              </ul>
-              <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+             </ul>
+             <div className="d-flex" >          
+             <Link className="nav-link" to="/logout"><FaUserCircle />Salir</Link>
+             </div>
            </div>
          </div>
        </nav> : <></>}
